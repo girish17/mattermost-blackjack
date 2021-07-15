b015f77c291281930c9ed7a582d0deece206929f
girishm <girishm@posteo.net>
2021-07-15 14:28:07 +0530

    jpg images for cards and initial dealing

--- server/plugin.go
+++ server/plugin.go
@@ -4,47 +4,49 @@ import (
 	"fmt"
 	"github.com/mattermost/mattermost-server/v5/model"
 	"github.com/pkg/errors"
+	"math/rand"
 	"net/http"
 	"sync"
+	"time"
 
 	"github.com/mattermost/mattermost-server/v5/plugin"
 )
 
 const bjCommand = "blackjack"
 const bjBot = "blackjack-bot"
-var cards = map[string]int {"A of Hearts": 11, "2 of Hearts": 2, "3 of Hearts": 3, "4 of Hearts": 4, "5 of Hearts": 5,
-	"6 of Hearts": 6, "7 of Hearts": 7, "8 of Hearts": 8, "9 of Hearts": 9, "10 of Hearts": 10,
-	"J of Hearts": 10, "Q of Hearts": 10, "K of Hearts": 10,
-	"A of Spades": 11, "2 of Spades": 2, "3 of Spades": 3, "4 of Spades": 4, "5 of Spades": 5,
-	"6 of Spades": 6, "7 of Spades": 7, "8 of Spades": 8, "9 of Spades": 9, "10 of Spades": 10,
-	"J of Spades": 10, "Q of Spades": 10, "K of Spades": 10,
-	"A of Diamonds": 11, "2 of Diamonds": 2, "3 of Diamonds": 3, "4 of Diamonds": 4, "5 of Diamonds": 5,
-	"6 of Diamonds": 6, "7 of Diamonds": 7, "8 of Diamonds": 8, "9 of Diamonds": 9, "10 of Diamonds": 10,
-	"J of Diamonds": 10, "Q of Diamonds": 10, "K of Diamonds": 10,
-	"A of Clubs": 11, "2 of Clubs": 2, "3 of Clubs": 3, "4 of Clubs": 4, "5 of Clubs": 5, "6 of Clubs": 6,
-	"7 of Clubs": 7, "8 of Clubs": 8, "9 of Clubs": 9, "10 of Clubs": 10, "J of Clubs": 10, "Q of Clubs": 10,
-	"K of Clubs": 10}
+var cards = map[string]int {"ace_of_hearts": 11, "2_of_hearts": 2, "3_of_hearts": 3, "4_of_hearts": 4, "5_of_hearts": 5,
+	"6_of_hearts": 6, "7_of_hearts": 7, "8_of_hearts": 8, "9_of_hearts": 9, "10_of_hearts": 10,
+	"jack_of_hearts": 10, "queen_of_hearts": 10, "king_of_hearts": 10,
+	"ace_of_spades": 11, "2_of_spades": 2, "3_of_spades": 3, "4_of_spades": 4, "5_of_spades": 5,
+	"6_of_spades": 6, "7_of_spades": 7, "8_of_spades": 8, "9_of_spades": 9, "10_of_spades": 10,
+	"jack_of_spades": 10, "queen_of_spades": 10, "king_of_spades": 10,
+	"ace_of_diamonds": 11, "2_of_diamonds": 2, "3_of_diamonds": 3, "4_of_diamonds": 4, "5_of_diamonds": 5,
+	"6_of_diamonds": 6, "7_of_diamonds": 7, "8_of_diamonds": 8, "9_of_diamonds": 9, "10_of_diamonds": 10,
+	"jack_of_diamonds": 10, "queen_of_diamonds": 10, "king_of_diamonds": 10,
+	"ace_of_clubs": 11, "2_of_clubs": 2, "3_of_clubs": 3, "4_of_clubs": 4, "5_of_clubs": 5, "6_of_clubs": 6,
+	"7_of_clubs": 7, "8_of_clubs": 8, "9_of_clubs": 9, "10_of_clubs": 10, "jack_of_clubs": 10, "queen_of_clubs": 10,
+	"king_of_clubs": 10}
 
-var playingCardUnicode = map[string]string {"A of Hearts": "\U0001F0B1", "2 of Hearts": "\U0001F0B2", "3 of Hearts": "\U0001F0B3",
-	"4 of Hearts": "\U0001F0B4", "5 of Hearts": "\U0001F0B5", "6 of Hearts": "\U0001F0B6",
-	"7 of Hearts": "\U0001F0B7", "8 of Hearts": "\U0001F0B8", "9 of Hearts": "\U0001F0B9",
-	"10 of Hearts": "\U0001F0BA", "J of Hearts": "\U0001F0BB", "Q of Hearts": "\U0001F0BD",
-	"K of Hearts": "\U0001F0BE",
-	"A of Spades": "\U0001F0A1", "2 of Spades": "\U0001F0A2", "3 of Spades": "\U0001F0A3",
-	"4 of Spades": "\U0001F0A4", "5 of Spades": "\U0001F0A5", "6 of Spades": "\U0001F0A6",
-	"7 of Spades": "\U0001F0A7", "8 of Spades": "\U0001F0A8", "9 of Spades": "\U0001F0A9",
-	"10 of Spades": "\U0001F0AA", "J of Spades": "\U0001F0AB", "Q of Spades": "\U0001F0AD",
-	"K of Spades": "\U0001F0AE",
-	"A of Diamonds": "\U0001F0C1", "2 of Diamonds": "\U0001F0C2", "3 of Diamonds": "\U0001F0C3",
-	"4 of Diamonds": "\U0001F0C4", "5 of Diamonds": "\U0001F0C5", "6 of Diamonds": "\U0001F0C6",
-	"7 of Diamonds": "\U0001F0C7", "8 of Diamonds": "\U0001F0C8", "9 of Diamonds": "\U0001F0C9",
-	"10 of Diamonds": "\U0001F0CA", "J of Diamonds": "\U0001F0CB", "Q of Diamonds": "\U0001F0CD",
-	"K of Diamonds": "\U0001F0CE",
-	"A of Clubs": "\U0001F0D1", "2 of Clubs": "\U0001F0D2", "3 of Clubs": "\U0001F0D3",
-	"4 of Clubs": "\U0001F0D4", "5 of Clubs": "\U0001F0D5", "6 of Clubs": "\U0001F0D6",
-	"7 of Clubs": "\U0001F0D7", "8 of Clubs": "\U0001F0D8", "9 of Clubs": "\U0001F0D9",
-	"10 of Clubs": "\U0001F0DA", "J of Clubs": "\U0001F0DB", "Q of Clubs": "\U0001F0DD",
-	"K of Clubs": "\U0001F0DE"}
+var playingCards = []string {"ace_of_hearts", "2_of_hearts", "3_of_hearts",
+	"4_of_hearts", "5_of_hearts", "6_of_hearts",
+	"7_of_hearts", "8_of_hearts", "9_of_hearts",
+	"10_of_hearts", "jack_of_hearts", "queen_of_hearts",
+	"king_of_hearts",
+	"ace_of_spades", "2_of_spades", "3_of_spades",
+	"4_of_spades", "5_of_spades", "6_of_spades",
+	"7_of_spades", "8_of_spades", "9_of_spades",
+	"10_of_spades", "jack_of_spades", "queen_of_spades",
+	"king_of_spades",
+	"ace_of_diamonds", "2_of_diamonds", "3_of_diamonds",
+	"4_of_diamonds", "5_of_diamonds", "6_of_diamonds",
+	"7_of_diamonds", "8_of_diamonds", "9_of_diamonds",
+	"10_of_diamonds", "jack_of_diamonds", "queen_of_diamonds",
+	"king_of_diamonds",
+	"ace_of_clubs", "2_of_clubs", "3_of_clubs",
+	"4_of_clubs", "5_of_clubs", "6_of_clubs",
+	"7_of_clubs", "8_of_clubs", "9_of_clubs",
+	"10_of_clubs", "jack_of_clubs", "queen_of_clubs",
+	"king_of_clubs"}
 
 // Plugin implements the interface expected by the Mattermost server to communicate between the server and plugin processes.
 type Plugin struct {
@@ -77,13 +79,49 @@ func (p *Plugin) OnActivate() error {
 }
 
 func (p *Plugin) ExecuteCommand(c *plugin.Context, args *model.CommandArgs) (*model.CommandResponse, *model.AppError){
+	rand.Seed(time.Now().UnixNano())
+	var result = ""
+	var card1 = playingCards[rand.Intn(len(playingCards))]
+	var card2 = playingCards[rand.Intn(len(playingCards))]
+	var score = cards[card1] + cards[card2]
+	if score == 22 {
+		score = 12
+	}
+	if score == 21 {
+		result = "\n**BlackJack. Congratulations, You won!**"
+	}
 	var cmdResp *model.CommandResponse
 	cmdResp = &model.CommandResponse{
 		ResponseType: model.COMMAND_RESPONSE_TYPE_IN_CHANNEL,
-		Text: "Your cards are: ",
+		Text: "!["+ card1 + "](" + getPluginURL(p.GetSiteURL()) + "/public/jpg-cards/" + card1 + ".jpg)!["+ card2 +"](" + getPluginURL(p.GetSiteURL()) + "/public/jpg-cards/" + card2 + ".jpg)" + result,
 		Username: bjBot,
+		Props: nil,
 	}
 	return cmdResp, nil
+}
+
+func getAttachmentJSON(pluginURL string) string {
+	return `{
+		"attachments": [
+           {
+			 "text": "Hit?",
+			 "integration": {
+				"url": "` + pluginURL + `"/hit",
+				"context": {
+					"action": "hit"
+                 }
+              }
+		   },
+           {
+             "text": "Stay!",
+             "integration": {
+             "url": "` + pluginURL + `"/stay",
+             "context": {
+                 "action": "stay"
+             }
+           }
+		]
+	}`
 }
 
 func (p *Plugin) GetSiteURL() string {
@@ -93,6 +131,10 @@ func (p *Plugin) GetSiteURL() string {
 		siteURL = *ptr
 	}
 	return siteURL
+}
+
+func getPluginURL(siteURL string) string {
+	return siteURL + "/plugins/com.girishm.mattermost-blackjack"
 }
 
 func createBJCommand(siteURL string) *model.Command {
